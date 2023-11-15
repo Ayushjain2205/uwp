965c0ccf3bd92280747788b7995cc2d0dc5cb10e
Ayush Jain <ayushjain2205@gmail.com>
2023-11-15 22:54:17 +0530

    Init project

---
+++ App.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
+
+const Congrats = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.circleContainer}>
+        <View style={styles.circle}>
+          {/* SVG content will be replaced with equivalent React Native components */}
+        </View>
+      </View>
+      <View style={styles.textContainer}>
+        <Text style={styles.text}>Congrats!</Text>
+        <Text style={styles.text}>You are a member</Text>
+        <Text style={[styles.text, styles.fontMedium]}>
+          RATION CARD INITIATIVE BY NFSA
+        </Text>
+      </View>
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Done</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  circleContainer: {
+    marginTop: 140,
+  },
+  circle: {
+    width: 85,
+    height: 85,
+    borderRadius: 42.5,
+    backgroundColor: '#1A2DD9',
+    borderColor: 'white',
+    borderWidth: 4,
+  },
+  textContainer: {
+    width: 336,
+    marginTop: 32,
+  },
+  text: {
+    fontSize: 24,
+    textAlign: 'center',
+  },
+  fontMedium: {
+    fontWeight: 'bold',
+    lineHeight: 0.6, // Adjust as needed
+  },
+  button: {
+    paddingHorizontal: 39,
+    paddingVertical: 10,
+    backgroundColor: '#1A2DD9',
+    borderRadius: 8,
+    marginTop: 155,
+  },
+  buttonText: {
+    fontSize: 16,
+    color: 'white',
+    fontWeight: 'bold',
+  },
+});
+
+export default Congrats;
