739b0e7dbcb8056e763f655fb4db3e4a2c4dd452
Ayush Jain <ayushjain2205@gmail.com>
2023-11-17 23:45:28 +0530

    Add basic bottom navbar

--- App.tsx
+++ App.tsx
@@ -3,6 +3,9 @@ import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Tick from './assets/svgs/Tick';
+import HouseIcon from './assets/svgs/HouseIcon';
+import WalletIcon from './assets/svgs/WalletIcon';
+import ProfileIcon from './assets/svgs/ProfileIcon';
 
 // Define the CongratsScreen component
 const CongratsScreen = () => {
@@ -27,6 +30,50 @@ const CongratsScreen = () => {
   );
 };
 
+const CongratsScreen1 = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.circleContainer}>
+        <View style={styles.circle}>
+          <Tick />
+        </View>
+      </View>
+      <View style={styles.textContainer}>
+        <Text style={styles.text}>Congrats 1!</Text>
+        <Text style={styles.text}>You are a member</Text>
+        <Text style={[styles.text, styles.fontMedium]}>
+          RATION CARD INITIATIVE BY NFSA
+        </Text>
+      </View>
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Done</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const CongratsScreen2 = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.circleContainer}>
+        <View style={styles.circle}>
+          <Tick />
+        </View>
+      </View>
+      <View style={styles.textContainer}>
+        <Text style={styles.text}>Congrats 2!</Text>
+        <Text style={styles.text}>You are a member</Text>
+        <Text style={[styles.text, styles.fontMedium]}>
+          RATION CARD INITIATIVE BY NFSA
+        </Text>
+      </View>
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Done</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
 // Create a Bottom Tab Navigator
 const Tab = createBottomTabNavigator();
 
@@ -37,17 +84,51 @@ const App = () => {
         screenOptions={{
           headerShown: false,
           tabBarShowLabel: false,
-          tabBarStyle: {backgroundColor: '#1A2DD9'}, // Customize your tab bar style
+          tabBarStyle: {
+            backgroundColor: '#FFFFFF', // or any color you want for the tab bar
+            borderTopColor: '#1A2DD9',
+            borderTopWidth: 2,
+            height: 80,
+            alignItems: 'center', // Center items vertically
+            justifyContent: 'center', // Center items vertically
+          },
         }}>
         {/* Here, you add your tabs, for example, the Congrats screen as a tab */}
         <Tab.Screen
           name="Congrats"
           component={CongratsScreen}
-          options={
-            {
-              // tabBarIcon is where you put your icon component
-            }
-          }
+          options={{
+            tabBarIcon: ({focused}) => (
+              <View style={styles.iconContainer}>
+                <HouseIcon />
+                {focused && <View style={styles.activeDot} />}
+              </View>
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Congrats1"
+          component={CongratsScreen1}
+          options={{
+            tabBarIcon: ({focused}) => (
+              <View style={styles.iconContainer}>
+                <WalletIcon />
+                {focused && <View style={styles.activeDot} />}
+              </View>
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Congrats2"
+          component={CongratsScreen2}
+          options={{
+            tabBarIcon: ({focused}) => (
+              <View style={styles.iconContainer}>
+                <ProfileIcon />
+                {focused && <View style={styles.activeDot} />}
+              </View>
+            ),
+          }}
         />
         {/* Add more Tab.Screen components for other tabs */}
       </Tab.Navigator>
@@ -100,6 +181,20 @@ const styles = StyleSheet.create({
     color: 'white',
     fontWeight: 'bold',
     textAlign: 'center', // Ensure text is centered within the button
+  },
+  iconContainer: {
+    width: 24, // Match the width of your SVG icons
+    height: 24, // Match the height of your SVG icons
+    alignItems: 'center', // Centers the blue dot horizontally below the icon
+  },
+  activeDot: {
+    width: 6,
+    height: 6,
+    borderRadius: 3,
+    backgroundColor: '#1A2DD9',
+    position: 'absolute',
+    bottom: -10, // Adjust the distance below the icon
+    left: 9, // Centers the dot below the icon (assuming the icon is 24px wide)
   },
 });
 
